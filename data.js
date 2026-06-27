685f82cb20738ef22980fcb0eca760a6dc044d89
discoveringchiayi <discoveringchiayi@gmail.com>
2026-06-27 14:52:03 +0800

    Add virtual tour

---
+++ data.js
@@ -0,0 +1,225 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-home",
+      "name": "Home",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": -0.4785527550082662,
+        "pitch": -0.11551661479118813,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4992072429208605,
+          "pitch": -0.02537482133887181,
+          "rotation": 7.853981633974483,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": 1.1000069448117173,
+        "pitch": -0.01299888698324736,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2128210012299476,
+          "pitch": 0.046799221839069816,
+          "rotation": 0,
+          "target": "0-home"
+        },
+        {
+          "yaw": 1.6641264140503456,
+          "pitch": 0.04092994153219642,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6503260523834484,
+          "pitch": 0.04550999708389192,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": 0.1639872135774123,
+          "pitch": 0.3274968128341591,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-4",
+      "name": "4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": -0.10811285531168124,
+        "pitch": 0.17552908166034165,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.015528615957908443,
+          "pitch": 0.08392221687010348,
+          "rotation": 0,
+          "target": "4-5"
+        },
+        {
+          "yaw": -3.0321338857266547,
+          "pitch": 0.08121850843401113,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5",
+      "name": "5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1376,
+      "initialViewParameters": {
+        "yaw": 0.5321593192485619,
+        "pitch": 0.01616604224345508,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5279449245328358,
+          "pitch": 0.052073077195943895,
+          "rotation": 7.853981633974483,
+          "target": "3-4"
+        },
+        {
+          "yaw": -1.1488069878241554,
+          "pitch": -0.02051480925659277,
+          "rotation": 17.27875959474387,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "洲南鹽場",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
